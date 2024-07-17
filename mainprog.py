97cad4fd999ff6fdebace962fde22d8eb3890340
blurillusion <thisislife@live.com.sg>
2024-07-17 10:09:29 +0800

    Update mainprog_addemp.py

--- mainprog.py
+++ mainprog.py
@@ -1,4 +1,6 @@
 # creating a file, emp4 does not exist in this nb
 from empClass import Employee
 emp4 = Employee("Xavier", "Ho", "M77777")
+emp5 = Employee("David", "Ho", "M11111")
 print(emp4.first_name)
+print(emp4.first_name, emp5.first_name)
